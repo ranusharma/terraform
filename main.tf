a792a0ca6ed6cef4a3e2d88c4174db5f24e19c7f
ranusharma <ranu.sharma@bristlecone.com>
2025-05-06 15:50:43 +0530

    update

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ provider "okta" {
   base_url = "okta.com"
   api_token = var.api_token
 }
+resource "okta_user" "new_user" {
+  first_name = "Auto"
+  last_name  = "User"
+  email      = var.new_user_email
+  login      = var.new_user_email
+  password   = "4%VM|(8hwA$$"
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "api_token" {
   type = string
 }
+variable "new_user_email" {
+  type        = string
+  description = "Email address of the new user"
+}
